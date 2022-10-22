9baafe92dd9640b4c240f506a254c27acee52745
agowriss <75127492+agowriss@users.noreply.github.com>
2022-10-22 10:32:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,18 +22,21 @@ fruits_to_display = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_display)
 
+#Create a repeatable code block (called a funtion)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized  
+
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi') <-- Old code
-#streamlit.write('The user entered ', fruit_choice) <-- Old code
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-        fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)
 
 except URLError as e:
     streamlit.error()
@@ -54,4 +57,3 @@ streamlit.write('Thanks for adding ', fruit_choice)
 
 #This will not go correct, but go with this for now
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-    
